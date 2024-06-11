4dbdcb99ad27fdc64c9c4e1f4439ddaa70a8e2cf
Harshasiddi <harshavardhan9805@gmail.com>
2024-06-11 13:37:20 -0400

    Update main.tf

---
+++ terraform /Project/modules/keyvault/main.tf
@@ -0,0 +1,16 @@
+# Fetch current Azure client configuration
+data "azurerm_client_config" "current" {}
+
+# Define the Azure Key Vault
+resource "azurerm_key_vault" "main" {
+  name                        = var.keyvault_name
+  location                    = var.location
+  resource_group_name         = var.resource_group_name
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  purge_protection_enabled    = false
+  sku_name                    = "premium"
+  soft_delete_retention_days  = 7
+  enable_rbac_authorization   = true
+
+}
